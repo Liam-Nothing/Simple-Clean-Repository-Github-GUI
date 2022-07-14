2865675becc337b6bf2efcadd9083fb23bc31cec
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-07-15 00:50:16 +0200

    [Bugfix] Work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 link = input("Link : ")
 directory = link.split("/")[-1]
@@ -6,4 +7,8 @@ directory = link.split("/")[-1]
 
 os.system('cmd /k "git clone --mirror ' + link + ' & exit"')
 os.system('cmd /k "bfg-1.14.0.jar --replace-text passwords.txt  ' + directory + ' & exit"')
-os.rmdir(directory)
+os.chdir(directory + ".bfg-report")
+os.system('cmd /k "git push --force & exit"')
+# os.chdir("..")
+# shutil.rmtree(os.curdir + "\\" + directory)
+# shutil.rmtree(os.curdir + "\\" + directory + ".bfg-report")
